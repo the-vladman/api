1fcbe89f2d498d957ca4b21e960a7137fe1c230e
Ben Cessa <ben@pixative.com>
2016-03-04 16:34:57 -0500

    Update AirQuality Agent to the new design

--- agent/airquality/lib/airquality.js
+++ agent/airquality/lib/airquality.js
@@ -5,7 +5,6 @@
 var BudaCSVAgent = require( '../../csv/lib/buda_agent_csv' );
 var _ = require( 'underscore' );
 var util = require( 'util' );
-var info = require( '../package' );
 var FeedSpec = require( './feed.spec' );
 
 // Supported pollutants to store
@@ -37,7 +36,6 @@ var indices = [
 // Constructor method
 // Extends base CSV Agent definition
 function AirQualityAgent( conf ) {
-  this.log( info.name + ' ' + info.version );
   BudaCSVAgent.call( this, conf );
 }
 util.inherits( AirQualityAgent, BudaCSVAgent );
@@ -69,10 +67,14 @@ AirQualityAgent.prototype.transform = function( record ) {
   var doc = new FeedSpec.FeedEntry();
   var date = new Date( record.fecha + ' ' + record.hora );
 
+  if( ! record.nom || record.nom === '' ) {
+    this.emit( 'error', record );
+  }
+
   /* eslint camelcase:0 */
   doc.stations[ 0 ].id = record.cve;
   doc.stations[ 0 ].name = record.nom;
-  doc.stations[ 0 ].source_id = _findSourceID( record.cve );
+  doc.stations[ 0 ].source_id = _findSourceID( record.cve ) || '';
   doc.stations[ 0 ].location.lat = record.lat;
   doc.stations[ 0 ].location.lon = record.lon;
   if( _.indexOf( pollutants, record.para ) >= 0 ) {
