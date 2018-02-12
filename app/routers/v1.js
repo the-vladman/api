e5640b3c61eb3152a636bfe6f2aa497250f7cfdf
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2018-02-12 10:12:51 -0600

    Remove Invalid Operations
    Forbidden Methods

--- app/routers/v1.js
+++ app/routers/v1.js
@@ -8,23 +8,16 @@ var router = new express.Router();
 module.exports = function(options) {
   // Load required controller
   var api = require('../controllers/api.v1')(options);
-
-  // Auth routes
-  router.post('/auth', api.registerConsumer);
-  router.get('/auth/:id', api.getConsumerInfo);
-  router.post('/auth/:id/key', api.addConsumerKey);
-  router.delete('/auth/:id/key/:keyId', api.delConsumerKey);
-
   // DCAT metadata
   router.get('/catalog.json', api.catalogInfo);
 
   // Data CRUD operations
   router.get('/:dataCollection', api.runQuery);
   router.get('/:dataCollection/:docId', api.getDocument);
-  router.post('/:dataCollection', api.registerDocument);
-  router.put('/:dataCollection/:docId', api.updateDocument);
-  router.patch('/:dataCollection/:docId', api.updateDocument);
-  router.delete('/:dataCollection/:docId', api.delDocument);
+  router.post('/:dataCollection', api.forbidden);
+  router.put('/:dataCollection/:docId', api.forbidden);
+  router.patch('/:dataCollection/:docId', api.forbidden);
+  router.delete('/:dataCollection/:docId', api.forbidden);
 
   return router;
 };
